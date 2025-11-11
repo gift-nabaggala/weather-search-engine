b3a12860472fe5a848fb37d3d54ec21e507486db
gift-nabaggala <nabaggalagift20@gmail.com>
2025-11-11 13:29:24 +0300

    Deleted files and modified index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 const city = "Paris";
-const apiKey = process.env.apiKey;
+const apiKey = "344fd2b82ccb20at17163b1f587bo614";
 
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+axios.get(apiUrl).then(displayTemperature);
